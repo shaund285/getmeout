03572cc9f36dbdb7b60e30f962d5e0b55db0bd38
Roi Nir <roinir237@gmail.com>
2013-10-26 17:05:32 +0100

    initial

---
+++ src/com/team14/getmeout/BudgetFragment.java
@@ -0,0 +1,16 @@
+package com.team14.getmeout;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BudgetFragment extends Fragment {
+	 @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.budget_fragment_layout, container, false);
+	}
+}
---
+++ src/com/team14/getmeout/MainActivity.java
@@ -0,0 +1,32 @@
+package com.team14.getmeout;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+
+public class MainActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		BudgetFragment budgetFragment = new BudgetFragment();
+		
+		FragmentManager fragmentManager = getFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+		
+		fragmentTransaction.add(R.id.fragment_container, budgetFragment);
+		fragmentTransaction.commit();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
