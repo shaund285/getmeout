a4e99fb1d2c2e0f17b356f1ec059f86b952ec0de
Shaun Dowling <shaunmdowling@gmail.com>
2013-10-26 23:23:13 +0100

    Fragments inside fragments

--- src/com/team14/getmeout/EventDetailsFragment.java
+++ src/com/team14/getmeout/EventDetailsFragment.java
@@ -6,11 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class EventDetailsFragmant extends Fragment {
+public class EventDetailsFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.event_fragment_layout, container, false);
+		View view = inflater.inflate(R.layout.event_details_layout, container, false);
 		
 		return view;
 	}
--- src/com/team14/getmeout/MainActivity.java
+++ src/com/team14/getmeout/MainActivity.java
@@ -6,12 +6,6 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
 public class MainActivity extends Activity {
 
 	@Override
@@ -19,27 +13,10 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-
-		MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-		
-		GoogleMap map = mapFragment.getMap();
-		map.getUiSettings().setAllGesturesEnabled(false);
-		map.getUiSettings().setZoomControlsEnabled(false);
-		
-		LatLng sydney = new LatLng(-33.867, 151.206);
-		
-//		map.setMyLocationEnabled(true);
-		map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
-		
-		map.addMarker(new MarkerOptions()
-			.title("Sydney")
-			.snippet("The most populos city in Australia.")
-			.position(sydney));		
-		
-//		Fragment fragment = new BudgetFragment();
-//		FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-//		fragmentTransaction.add(R.id.fragment_container, fragment);
-//		fragmentTransaction.commit();
+		Fragment fragment = new EventPageFragment();
+		FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+		fragmentTransaction.add(R.id.fragment_container, fragment);
+		fragmentTransaction.commit();
 	}
 
 	@Override
