1b3c32983544de5466b938ba102c6643132bfafc
qwattash <mzz.lrd@gmail.com>
2013-10-26 22:49:49 +0100

    BudgetFragment complete features added

--- src/com/team14/getmeout/BudgetFragment.java
+++ src/com/team14/getmeout/BudgetFragment.java
@@ -1,5 +1,7 @@
 package com.team14.getmeout;
 
+import java.util.Date;
+
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,12 +15,6 @@ import android.app.FragmentTransaction;
 
 public class BudgetFragment extends Fragment implements OnClickListener{
 	
-	public static enum PlanningMode {
-		PLANNING_NOW,
-		PLANNING_WEEK,
-		PLANNING_TODAY
-	}
-	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -28,7 +24,9 @@ public class BudgetFragment extends Fragment implements OnClickListener{
         //add button callback
         Button b = (Button) v.findViewById(R.id.budget_fragment_layout_go);
         b.setOnClickListener(this);
-        
+        //init default search mode
+        b = (Button) v.findViewById(R.id.budget_fragment_mode);
+        b.setOnClickListener(this);
 		return v;
 	}
 	
@@ -37,25 +35,63 @@ public class BudgetFragment extends Fragment implements OnClickListener{
 	 */
 	@Override
 	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.budget_fragment_layout_go:
+				this.toEventList();
+				break;
+			case R.id.budget_fragment_mode:
+				this.changeMode(v);
+				break;
+		}
+	}
+	
+	private void toEventList() {
 		//move to list fragment if a good amount of money have been inserted in the 
-		//text field
-		//get text field info as float
-		float budget;
-		EditText budgetField = (EditText) this.getView().findViewById(R.id.budget_fragment_budgetText);
-		try {
-			budget = Float.valueOf(budgetField.getText().toString());
+				//text field
+				//get text field info as float
+				float budget;
+				EditText budgetField = (EditText) this.getView().findViewById(R.id.budget_fragment_budgetText);
+				try {
+					budget = Float.valueOf(budgetField.getText().toString());
+				}
+				catch (NumberFormatException e) {
+					//invalid number format, notify with a popup
+					budgetField.setText("Invalid Number");
+					return;
+				}
+				//get planning mode
+				Date start = new Date();
+				Date end = new Date();
+				Button modeButton = (Button) this.getView().findViewById(R.id.budget_fragment_mode);
+				String weekString = this.getActivity().getResources().getString(R.string.budget_fragment_mode_week);
+				if (modeButton.getText().equals(weekString)) {
+					//if serching for this week's events add 7 days
+					end.setDate(start.getDay() + 7);
+				}
+				else {
+					//if serching for today'sevents add 24h
+					end.setDate(end.getDate() + 1);
+				}
+				//switch fragment
+				Fragment listFragment = new EventListFragment(budget, start, end);
+				budgetField.setText(start.toGMTString() + " " + end.toGMTString());
+				FragmentManager fm = this.getFragmentManager();
+				FragmentTransaction ft = fm.beginTransaction();
+				ft.replace(R.id.fragment_container, listFragment);
+				//ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+				ft.addToBackStack(null);
+				ft.commit();
+	}
+	
+	private void changeMode(View v) {
+		Button modeButton = (Button) v.findViewById(R.id.budget_fragment_mode);
+		String weekString = this.getActivity().getResources().getString(R.string.budget_fragment_mode_week);
+		if (modeButton.getText().equals(weekString)) {
+			modeButton.setText(R.string.budget_fragment_mode_today);
 		}
-		catch (NumberFormatException e) {
-			//invalid number format, notify with a popup
-			budgetField.setText("Invalid Number");
-			return;
+		else {
+			modeButton.setText(R.string.budget_fragment_mode_week);
 		}
-		//get planning mode
 		
-		Fragment listFragment = new EventListFragment(budget);
-		FragmentManager fm = this.getFragmentManager();
-		FragmentTransaction ft = fm.beginTransaction();
-		ft.replace(R.id.fragment_container, listFragment);
-		ft.commit();	
 	}
 }
--- src/com/team14/getmeout/EventListFragment.java
+++ src/com/team14/getmeout/EventListFragment.java
@@ -1,14 +1,14 @@
 package com.team14.getmeout;
 
-import android.app.ListFragment;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class EventListFragment extends ListFragment{
+public class EventListFragment extends Fragment{
 	 @Override
 	    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-	        return inflater.inflate(R.layout.budget_fragment_layout, container, false);
+	        return inflater.inflate(R.layout.event_list_fragment_layout, container, false);
 		}
 }
