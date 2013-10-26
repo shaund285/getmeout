4b197f95b6729ef1d1d14df4523b319f341e5d41
qwattash <mzz.lrd@gmail.com>
2013-10-26 20:44:28 +0100

    BudgetFragment first commit

--- src/com/team14/getmeout/BudgetFragment.java
+++ src/com/team14/getmeout/BudgetFragment.java
@@ -5,12 +5,60 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 
-public class BudgetFragment extends Fragment {
-	 @Override
+
+public class BudgetFragment extends Fragment implements OnClickListener{
+	
+	public static enum PlanningMode {
+		PLANNING_NOW,
+		PLANNING_WEEK,
+		PLANNING_TODAY
+	}
+	
+	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.budget_fragment_layout, container, false);
+        View v = inflater.inflate(R.layout.budget_fragment_layout, container, false);
+        
+        //add button callback
+        Button b = (Button) v.findViewById(R.id.budget_fragment_layout_go);
+        b.setOnClickListener(this);
+        
+		return v;
 	}
+	
+	/**
+	 * OnClickListener interface
+	 */
+	@Override
+	public void onClick(View v) {
+		//move to list fragment if a good amount of money have been inserted in the 
+		//text field
+		//get text field info as float
+		float budget;
+		EditText budgetField = (EditText) this.getView().findViewById(R.id.budget_fragment_budgetText);
+		try {
+			budget = Float.valueOf(budgetField.getText().toString());
+		}
+		catch (NumberFormatException e) {
+			//invalid number format, notify with a popup
+			budgetField.setText("Invalid Number");
+			return;
+		}
+		//get planning mode
+		
+		Fragment listFragment = null; new EventListFragment(budget);
+		FragmentManager fm = this.getFragmentManager();
+		FragmentTransaction ft = fm.beginTransaction();
+		ft.replace(R.id.fragment_container, listFragment);
+		ft.commit();
+	}
+	
+	
 }
