db53ee2902ac6b5670782d172d790ac5e66f7bb6
Roi Nir <roinir237@gmail.com>
2013-10-27 01:21:34 +0100

    contacts page 'skeleton'

--- src/com/team14/getmeout/MainActivity.java
+++ src/com/team14/getmeout/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		ContactListFragment budgetFragment = new ContactListFragment();
+		BudgetFragment budgetFragment = new BudgetFragment();
 		
 		FragmentManager fragmentManager = getFragmentManager();
 		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
